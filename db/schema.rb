d2b0284a04565bc91e2f10cf97194c231874119b
zbahati <bahatizirimwabagabo@gmai.com>
2023-11-20 15:28:35 +0200

    Ft modal
    - Create category model
    - Create entity model
    - Create categoryEntity model for many to many relationship

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,36 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_20_101323) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_20_130338) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.string "icon"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_categories_on_user_id"
+  end
+
+  create_table "category_entities", force: :cascade do |t|
+    t.bigint "category_id", null: false
+    t.bigint "entity_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["category_id"], name: "index_category_entities_on_category_id"
+    t.index ["entity_id"], name: "index_category_entities_on_entity_id"
+  end
+
+  create_table "entities", force: :cascade do |t|
+    t.string "name"
+    t.decimal "amount"
+    t.bigint "author_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["author_id"], name: "index_entities_on_author_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -32,4 +59,8 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_20_101323) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "categories", "users"
+  add_foreign_key "category_entities", "categories"
+  add_foreign_key "category_entities", "entities"
+  add_foreign_key "entities", "users", column: "author_id"
 end
