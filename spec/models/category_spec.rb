0cd1497b62771f89fd99f5f606bcc4c93579906c
zbahati <bahatizirimwabagabo@gmai.com>
2023-11-23 13:01:46 +0200

    Fix some bugs and setup for deployment

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,21 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe Category, type: :model do
-  it 'is valid with name and icon' do
+  before do
     @user = User.create(name: 'Roy Batty', email: 'roy@batty.com', password: 'password')
-    category = @user.categories.create(name: 'Bills', icon: 'image.jpg')
-    expect(category).to be_valid
+    @uploaded_file = Shrine.uploaded_file(
+      "id" => "12c3c9721d97ffe32bf825152781f2ee.png",
+      "storage" => "store",
+      "metadata" => {
+        "filename" => "transport.png",
+        "size" => 1572,
+        "mime_type" => "image/png"
+      }
+    )
+  end
+
+  it 'is valid with name and icon' do
+    @user.categories.create(name: 'Bills', image: @uploaded_file)
+    expect(Category.last).to be_valid
   end
 
   it 'is not valid without name' do
-    @user = User.create(name: 'Roy Batty', email: 'roy@batty.com', password: 'password')
-    category = @user.categories.create(name: '', icon: 'image.jpg')
+    category = @user.categories.create(name: '', image: @uploaded_file)
     expect(category).not_to be_valid
   end
 
-  it 'is not valid without icon' do
-    @user = User.create(name: 'Roy Batty', email: 'roy@batty.com', password: 'password')
-    category = @user.categories.create(name: 'Bills', icon: '')
+  it 'is not valid without image_data' do
+    category = @user.categories.create(name: 'Bills', image: nil)
     expect(category).not_to be_valid
   end
 end
